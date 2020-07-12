51b7de91475196114c216f24585b9cb0c330be65
Alireza <alireza.f220@gmail.com>
2020-07-12 11:01:47 +0430

    Update index.js

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ client.on('message', message =>{
   if(message.author.bot) return;
 
   try{
-    delete require.cache[require.resolve(`discordbot2020/commands/${cmd}.js`)];
+    delete require.cache[require.resolve(`./commands/${cmd}.js`)];
 
-    let commandFile = require(`discordbot2020/commands/${cmd}.js`);
+    let commandFile = require(`./commands/${cmd}.js`);
     commandFile.run(client,message , args);
 
   } catch (e) {
