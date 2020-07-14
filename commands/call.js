700303ff54e06f604161a0e1f03647e3a230ac7b
Alireza <alireza.f220@gmail.com>
2020-07-14 13:47:32 +0430

    Update call.js

--- commands/call.js
+++ commands/call.js
@@ -2,7 +2,7 @@ exports.run = (client,message,args,ops)=>{
     if(!args[0]) return message.channel.send('Please tell me who you want to call using @ method!');
 
     if(args[0]){
-        if (args[0].startsWith('@')) {
+        if (args[0].startsWith('**@**')) {
             if(!args[1]) return message.channel.send('please tell me how many times you want to call!');
             if(!isNaN(args[1])) return message.channel.send('please put valid number!');
             
