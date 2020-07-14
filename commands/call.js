5014fabad6e557858750d955a3fe579ac2364795
Alireza <alireza.f220@gmail.com>
2020-07-14 14:09:27 +0430

    update

--- commands/call.js
+++ commands/call.js
@@ -5,8 +5,8 @@ exports.run = (client,message,args,ops)=>{
         if (args[0].includes('@')) {
             if(!args[1]) return message.channel.send('please tell me how many times you want to call!');
             if(isNaN(args[1])) return message.channel.send('please put valid number!');
-            const limit = args[1]+1;
-            var index = 0;
+            const limit = args[1].content +1;
+            var index = 1;
 
             while (index < limit) {
                 message.channel.send(`${args[0]} you are being called XD`);
