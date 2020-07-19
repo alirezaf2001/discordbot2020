0314eacb8c279ee2c5c41d3ef0bc19db892063ff
Alireza <alireza.f220@gmail.com>
2020-07-19 12:53:20 +0430

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,9 +10,9 @@ exports.run = (client, message, args , ops) =>{
          "-stop\t\tStops the song and delete the queue list\n\`-stop\`\n",
          "-volume\t\tChanges the volume between 1 to 200\n\`-volume 150\`\n"
     ]
-    let resp = `${commands[0]}`;
+    let resp =commands[0];
     for (let index = 1; index < commands.lenght+1; index++) {
-        resp += `${commands[index]}`;
+        resp += commands[index];
     }
 
     message.channel.send(resp);
