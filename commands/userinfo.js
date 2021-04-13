1929666abf8776951229d9c8c9628c2d511dab0c
alirezaf2001 <alireza.f220@gmail.com>
2021-04-13 14:24:06 +0430

    Create userinfo.js

---
+++ commands/userinfo.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+const Canvas = require("canvas");
+
+exports.run = (client , message , args)=> {
+    if(!args[0])
+    {
+        var user = message.author;
+    }else
+    {
+        var user = message.mentions.users.first() || client.users.cache.get(args[0]);
+    }
+    var member = message.guild.member(user);
+
+    const canvas = Canvas.createCanvas(500,200);
+    const ctx = Canvas.getContext("2d");
+
+    const background = await Canvas.loadImage("https://i.pinimg.com/564x/40/ab/fb/40abfb383ab6502ab3f72ea3b20feb00.jpg")
+
+    ctx.drawImage(background,0,0,canvas.width,canvas.height);
+
+    ctx.beginPath();
+    ctx.arc(100,100,75,0,Math.PI * 2 , true);
+    ctx.closePath();
+    ctx.clip();
+
+
+    const avatar = await Canvas.loadImage(user.displayAvatarURL({format : "jpg"}))
+    ctx.drawImage(avatar,25,25,150,150);
+
+    const final = new Discord.MessageAttachment(canvas.toBuffer(),"userSlap.png");
+
+    return message.channel.send(final);
+}
