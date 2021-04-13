8614c4360c61133c98679a4755b780433b99472d
alirezaf2001 <alireza.f220@gmail.com>
2021-04-13 17:42:22 +0430

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -13,28 +13,20 @@ exports.run = async (client , message , args)=> {
     var member = message.guild.member(user);
 
     const canvas = Canvas.createCanvas(500,300);
-    const ctx1 = canvas.getContext("2d");
-    const ctx2 = canvas.getContext("2d");
+    const ctx = canvas.getContext("2d");
 
     const background = await Canvas.loadImage("slap.jpg")
 
-    ctx1.drawImage(background,0,0,canvas.width,canvas.height);
+    ctx.drawImage(background,0,0,canvas.width,canvas.height);
 
-    ctx1.beginPath();
-    ctx1.arc(350,95,75,0,Math.PI * 2 , true);
-    ctx1.closePath();
-    ctx1.clip();
 
-    ctx2.beginPath();
-    ctx2.arc(100,100,75,0,Math.PI * 2 , true);
-    ctx2.closePath();
-    ctx2.clip();
+
 
 
     const avatarUser = await Canvas.loadImage(user.displayAvatarURL({format : "jpg"}))
     const avatarVictim = await Canvas.loadImage(user.displayAvatarURL({format : "jpg"}))
-    ctx1.drawImage(avatarUser,275,20,150,150);
-    ctx2.drawImage(avatarVictim,20,20,150,150);
+    ctx.drawImage(avatarUser,275,20,150,150);
+
     
     const final = new Discord.MessageAttachment(canvas.toBuffer(),"userSlap.png");
 
